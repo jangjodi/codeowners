e643a2d4ccfb8e90a770af5a0454cb363df332a4
Jodi Jang <jodi.jang@sentry.io>
2023-05-08 15:12:41 -0700

    chore: new file

--- file.py
+++ file.py
@@ -3,7 +3,7 @@ import sentry_sdk
 # Added something
 
 sentry_sdk.init(
-    dsn="http://7d00f17890264591a66254aa12052568@localhost:8000/12",
+    dsn="http://af2daeb6f96e417f81a9787e72cea45c@localhost:8000/12",
 
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
